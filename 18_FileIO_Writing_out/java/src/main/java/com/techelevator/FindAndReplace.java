261843da7e1109324eae5df216cc69c79630150b
Carson Hoff <hoffcarson@gmail.com>
2019-06-06 09:07:24 -0400

    Procedure

--- 18_FileIO_Writing_out/java/src/main/java/com/techelevator/FindAndReplace.java
+++ 18_FileIO_Writing_out/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -6,65 +6,49 @@ import java.util.Scanner;
 
 public class FindAndReplace {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		FindAndReplace findAndReplace = new FindAndReplace();
-		
+
 		System.out.println("Please enter the source file:");
 		String sourceFileString = in.nextLine();
-		
+
 		File file = new File(sourceFileString);
-		
+
 		System.out.println("Please enter the destination file:");
 		String destinationFileString = in.nextLine();
-		
+
+		// alices_adventures_in_wonderland.txt
 		File destinationFile = new File(destinationFileString);
-		
-		
+
 		System.out.print("Please enter the search word: ");
 		String searchWord = in.nextLine();
-		
 
 		System.out.print("Please enter the word to replace the search word with: ");
 		String replacementWord = in.nextLine();
 
 		try {
-				Scanner sourceFile = new Scanner(file);
+			Scanner sourceFile = new Scanner(file);
 
-				PrintWriter writer = new PrintWriter(destinationFile);
+			PrintWriter writer = new PrintWriter(destinationFile);
 
-				findAndReplace.writeCopy(searchWord, replacementWord, writer, sourceFile);		
-		}
-		catch (Exception e) {
-			
+			findAndReplace.writeCopy(searchWord, replacementWord, writer, sourceFile);
+		} catch (Exception e) {
+
 			System.out.println("You entered an invalid file");
 			System.exit(1);
 		}
-		
-		
+
 	}
-		
-		public void writeCopy (String searchWord, String replacementWord, PrintWriter writer, Scanner sourceFile) {
-			
-			while (sourceFile.hasNextLine()) {
-				String nextLine = sourceFile.nextLine();
-				nextLine = nextLine.replaceAll(searchWord, replacementWord);
-				writer.println(nextLine);
-				
-				
-			}
-			
-			
+
+	public void writeCopy(String searchWord, String replacementWord, PrintWriter writer, Scanner sourceFile) {
+
+		while (sourceFile.hasNextLine()) {
+			String nextLine = sourceFile.nextLine();
+			nextLine = nextLine.replaceAll(searchWord, replacementWord);
+			writer.println(nextLine);
+
 		}
+
 	}
-	
-
-	
-	
-
-
-
-
-
-
+}
